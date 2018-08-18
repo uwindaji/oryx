88419ef5c7afce4b33df4757ab84c474ac186278
uwindaji <utilisateur@MacBook-Pro-de-Utilisateur.local>
2018-08-18 11:26:27 +0200

    'vs code commit'

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ echo "add your commit"
 read commit
 git commit -m "'$commit'"
 
+git credential-manager delete lakhdar-rouibah
 git config --global user.name "uwindaji"
 git remote add origin https://uwindaji:enauoram29*@github.com/uwindaji/oryx.git
 git push -u origin master
