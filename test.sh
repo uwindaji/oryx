cc3040682a79b6d1188bbd1e237aeae4ec367675
Utilisateur <utilisateur@MacBook-Pro-de-Utilisateur.local>
2018-08-18 11:22:33 +0200

    'vs code commit'

--- test.sh
+++ test.sh
@@ -8,5 +8,6 @@ echo "add your commit"
 read commit
 git commit -m "'$commit'"
 
+git config --global user.name "uwindaji"
 git remote add origin https://uwindaji:enauoram29*@github.com/uwindaji/oryx.git
 git push -u origin master
